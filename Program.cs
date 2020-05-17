c99c5352424f13ca0e46e8c5e6937b6cc1f4d972
Gonan98 <andreg-16@hotmail.com>
2020-05-16 19:29:07 -0500

    Agregue un producto

--- Program.cs
+++ Program.cs
@@ -7,6 +7,14 @@ namespace Parcial
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Producto p1 = new Producto {
+                Id = 1,
+                Nombre = "Ryzen 5 2600",
+                Precio = 659.50
+            };
+
+            Console.WriteLine(p1.Nombre + " " + p1.Precio);
         }
     }
 }
