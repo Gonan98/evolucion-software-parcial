75881cd2d14f2f0ad6a28047509e63aa8af37957
Gonan98 <andreg-16@hotmail.com>
2020-05-16 19:34:50 -0500

    Agregado NombreCategoria como propiedad

--- Producto.cs
+++ Producto.cs
@@ -5,5 +5,6 @@ namespace Parcial
         public int Id { get; set; }
         public string Nombre { get; set; }
         public double Precio { get; set; }
+        public string NombreCategoria { get; set; }
     }
 }
